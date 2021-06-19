359551ab343a2c49bd85050242d9f0afe74cfcf2
Rodrigo Vieira Rocha Costa <c138421@corp.caixa.gov.br>
2021-06-19 19:05:23 -0300

    Criação do base-resource.service

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,12 @@
-export class Category{
+import { BaseResourceModel } from "src/app/shared/models/base-resource";
+
+export class Category extends BaseResourceModel{
+
   constructor(
     public id?:number,
     public name?:string,
     public description?: string
-  ){}
+  ){
+    super();
+  }
 }
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,16 +1,11 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
-
 import { SharedModule } from '../../shared/shared.module';
 
 import { CalendarModule } from 'primeng/calendar';
-
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 import { IMaskModule } from 'angular-imask';
-
-
-
 
 
 @NgModule({
